fbd5a5c8d72cfc38868ca044ea923deee863b7d4
cate <katyathegreatest@yandex.ru>
2022-12-18 01:13:40 +0300

    changed one line in header component;

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -6,7 +6,7 @@ const Header = () => {
   return (
     <header className="header">
       <nav className="nav container">
-        <a href="index.html" className="nav__logo">Chicherina</a>
+        <a href="index.html" className="nav__logo">Hello!</a>
 
         <div className={toggle ? "nav__menu show-menu" : "nav__menu"}>
           <ul className="nav__list grid">
