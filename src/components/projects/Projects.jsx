65730ed3f0c599cfeb761f90d868f0562178f988
katyeheisenberg <chimmi10kate@mail.ru>
2023-02-03 00:53:21 +0300

    added new project

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -216,6 +216,60 @@ const Projects = () => {
             </div>
           </div>
         </div>
+
+        <div className="services__content">
+          <div>
+            <i className="uil uil-web-grid services__icon"></i>
+            <h3 className="services__title">Photography page</h3>
+          </div>
+
+          <span className="services__button" onClick={() => toggleTab(5)}>
+            View more{" "}
+            <i className="uil uil-arrow-right services__button-icon"></i>
+          </span>
+
+          <div
+            className={
+              toggle === 5 ? "services__modal active-modal" : "services__modal"
+            }
+          >
+            <div className="services__modal-content">
+              <i
+                onClick={() => toggleTab(0)}
+                className="uil uil-times services__modal-close"
+              ></i>
+
+              <h3 className="services__modal-title"></h3>
+              <p className="services__modal-description">Link:</p>
+
+              <ul className="services__modal-services grid">
+                <li className="services__modal-service">
+                  <i className="uil uil-check-circle services__modal-icon"></i>
+                  <p className="services__modal-info">
+                    <a
+                      className="services__modal-info"
+                      href="https://github.com/katyeheisenberg/phpage2"
+                    >
+                      GitHub
+                    </a>
+                  </p>
+                </li>
+
+                <li className="services__modal-service">
+                  <i className="uil uil-check-circle services__modal-icon"></i>
+                  <p className="services__modal-info">
+                    <a
+                      className="services__modal-info"
+                      href="https://ph-page-kate.vercel.app/"
+                    >
+                      Project
+                    </a>
+                  </p>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
       </div>
     </section>
   );
