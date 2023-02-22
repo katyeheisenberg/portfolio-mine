3861580c66778dfdea678565714ff28dd712c89c
katyeheisenberg <chimmi10kate@mail.ru>
2023-02-22 19:00:46 +0300

    changed header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -6,7 +6,7 @@ const Header = () => {
   return (
     <header className="header">
       <nav className="nav container">
-        <a href="index.html" className="nav__logo">Hello!</a>
+        <span className="nav__logo">Hello!</span>
 
         <div className={toggle ? "nav__menu show-menu" : "nav__menu"}>
           <ul className="nav__list grid">
